12e1b3079eb0469319975320bbadc17709027868
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-02-18 21:31:45 +0100

    Refactoring

--- backend/tests/tokenizer/test_basic_tokenizer.py
+++ backend/tests/tokenizer/test_basic_tokenizer.py
@@ -9,41 +9,32 @@ if (project_path not in sys.path):
 print(sys.path)
 logging.basicConfig(level=logging.DEBUG)
 
-from tokenizer.basic_tokenizer import Token, BasicTokenizer, basic_rules
+from tokenizer.basic_tokenizer import BasicTokenizer
+
+TEST_CODE = """{
+    "port": 8999,
+    "target_url": "https://example.com",
+    "status": "working"
+    }"""
 
 class TestBasicTokenizer(unittest.TestCase):
 
     def test_tokenize(self):
         tokenizer = BasicTokenizer()
-        code = """{
-        "port": 8999,
-        "target_url": "https://example.com",
-        "status": "working"
-        }"""
-        tokens = tokenizer.tokenize(code)
+        tokens = tokenizer.tokenize(TEST_CODE)
         tokenizer.print_tokens(tokens)
         self.assertEqual(24, len(tokens))
 
     def test_detokenize(self):
         tokenizer = BasicTokenizer()
-        code = """{
-        "port": 8999,
-        "target_url": "https://example.com",
-        "status": "working"
-        }"""
-        tokens = tokenizer.tokenize(code)
+        tokens = tokenizer.tokenize(TEST_CODE)
         tokenizer.print_tokens(tokens)
         string = tokenizer.detokenize(tokens)
-        self.assertEqual(code, string)
+        self.assertEqual(TEST_CODE, string)
 
     def test_conversion_to_ids(self):
         tokenizer = BasicTokenizer()
-        code = """{
-        "port": 8999,
-        "target_url": "https://example.com",
-        "status": "working"
-        }"""
-        tokens = tokenizer.tokenize(code)
+        tokens = tokenizer.tokenize(TEST_CODE)
         tokenizer.print_tokens(tokens)
         ids = [tokenizer._convert_token_to_id(token) for token in tokens]
         print(ids)
@@ -51,12 +42,7 @@ class TestBasicTokenizer(unittest.TestCase):
     
     def test_conversion_to_tokens(self):
         tokenizer = BasicTokenizer()
-        code = """{
-        "port": 8999,
-        "target_url": "https://example.com",
-        "status": "working"
-        }"""
-        tokens = tokenizer.tokenize(code)
+        tokens = tokenizer.tokenize(TEST_CODE)
         ids = [tokenizer._convert_token_to_id(token) for token in tokens]
         print(ids)
         t2 = [tokenizer._convert_id_to_token(id_) for id_ in ids]   
