48489d8a9254b50fd3d2601712d1f979bb2e515b
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-02-27 18:35:48 +0100

    WebsocketService

--- frontend/src/app/websocket.service.ts
+++ frontend/src/app/websocket.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class WebsocketService {
 
-  constructor() { }
+    private socket!: WebSocketSubject<any>;
+
+    public connect(url: string): WebSocketSubject<any> {
+        if (!this.socket || this.socket.closed) {
+            this.socket = new WebSocketSubject(url);
+        }
+        return this.socket;
+    }
 }
