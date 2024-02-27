b4595611f4672381669beb38070001ed7d456077
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-02-27 18:48:35 +0100

    WebsocketService

--- frontend/src/app/websocket.service.ts
+++ frontend/src/app/websocket.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class WebsocketService {
+    private socket$: WebSocketSubject<any>;
+    private socketUrl = 'ws://localhost:8000/ws'; // URL do Twojego WebSocket endpoint
 
-    private socket!: WebSocketSubject<any>;
+    constructor() {
+        this.socket$ = new WebSocketSubject(this.socketUrl);
+    }
 
-    public connect(url: string): WebSocketSubject<any> {
-        if (!this.socket || this.socket.closed) {
-            this.socket = new WebSocketSubject(url);
-        }
-        return this.socket;
+    public connect(): WebSocketSubject<any> {
+        return this.socket$;
+    }
+
+    public send(message: any): void {
+        this.socket$.next(message);
+    }
+
+    public close(): void {
+        this.socket$.complete();
     }
 }
