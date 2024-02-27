5bdb671b81caf326915e0322645ae04efb7b597f
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-02-27 22:00:47 +0100

    Chat component

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from '../websocket.service'; // Załóżmy, że ten serwis już istnieje
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-chat',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+    selector: 'app-chat',
+    standalone: true,
+    imports: [ CommonModule, FormsModule ],
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+
+    messages: any[] = [];
+    newMessage = '';
+
+    constructor(private wsService: WebsocketService) { }
+
+    ngOnInit(): void {
+        this.wsService.connect().subscribe(
+            msg => {
+                this.messages.push(msg);
+            }
+        );
+    }
+
+    sendMessage(): void {
+        this.messages.push(this.newMessage);
+        console.log(this.newMessage)
+        console.log(this.messages)
+        if (this.newMessage.trim().length > 0) {
+            this.wsService.send({ text: this.newMessage });
+            this.newMessage = '';
+        }
+    }
 
 }
