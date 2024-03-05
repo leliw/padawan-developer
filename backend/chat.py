4327cce1d3321c67dc05bfa0eccd3f2d75a425bd
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-03-05 11:11:39 +0100

    Return files from output

--- backend/chat.py
+++ backend/chat.py
@@ -91,15 +91,23 @@ class Chat:
             ret.append({"channel": "bash_cmd", "text": f"$ {cmd}\n"})
             if out:
                 if s.out_regex:
-                    self.parse_out(out, s.out_regex)
+                    files = self.parse_out(out, s.out_regex)
+                    if files:
+                        ret.append({"channel": "files", "files": files})        
                 ret.append({"channel": "bash_out", "text": out})
             if err:
                 ret.append({"channel": "bash_err", "text": err})
         return ret
 
-    def parse_out(self, out: str, out_reqex: Optional[str]):
-        """Parse output with regex"""
+    def parse_out(self, out: str, out_reqex: Optional[str]) -> list[str]:
+        """Parse output with regex
+        
+        if property name ends with "_full_path" it will be returned as file.
+        """
         if out_reqex:
             match = re.search(out_reqex, out)
             if match:
-                self.params = self.params | match.groupdict()
+                params = match.groupdict()
+                self.params = self.params | params
+                return [v for k, v in params.items() if k.endswith("_full_path")]
+                
