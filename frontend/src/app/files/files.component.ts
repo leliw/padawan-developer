8a538ecd304b7d3d141d2ef0627d536d95c0ced9
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-03-07 13:26:41 +0100

    Restore open files

--- frontend/src/app/files/files.component.ts
+++ frontend/src/app/files/files.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { WebsocketService } from '../websocket.service';
-import { filter } from 'rxjs';
+import { filter, of } from 'rxjs';
 import { FileEditorComponent } from '../file-editor/file-editor.component';
+import { AppStateService } from '../app-state.service';
 
 
 @Component({
     selector: 'app-files',
     standalone: true,
-    imports: [MatTabsModule, FileEditorComponent ],
+    imports: [MatTabsModule, FileEditorComponent],
     templateUrl: './files.component.html',
     styleUrl: './files.component.css'
 })
@@ -16,7 +17,13 @@ export class FilesComponent {
 
     files = new Set<string>();
 
-    constructor(private wsService: WebsocketService) {
+    constructor(private wsService: WebsocketService, private appService: AppStateService) {
+        this.appService.get()
+            .subscribe(state => {
+                of(...state.chat_history)
+                    .pipe(filter(msg => (msg.channel == "files" && msg.files !== undefined)))
+                    .subscribe(msg => this.files = new Set([...this.files, ...(msg.files ?? [])]));
+            })
         this.wsService.connect()
             .pipe(filter(msg => (msg.channel == "files" && msg.files !== undefined)))
             .subscribe(msg => this.files = new Set([...this.files, ...(msg.files ?? [])]));
