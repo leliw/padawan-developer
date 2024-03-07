aa63d5d21a15c14f6cf905599416e27b23fa1c25
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-03-07 13:26:09 +0100

    Add CodeMirror for syntax highlighting and editing

--- frontend/src/app/file-editor/file-editor.component.ts
+++ frontend/src/app/file-editor/file-editor.component.ts
@@ -1,25 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/markdown/markdown';
 
 @Component({
-  selector: 'app-file-editor',
-  standalone: true,
-  imports: [],
-  templateUrl: './file-editor.component.html',
-  styleUrl: './file-editor.component.css'
+    selector: 'app-file-editor',
+    standalone: true,
+    imports: [FormsModule, CodemirrorModule],
+    templateUrl: './file-editor.component.html',
+    styleUrl: './file-editor.component.css'
 })
 export class FileEditorComponent implements OnChanges {
     @Input() file: string = '';
-
+    
     content = "";
 
     constructor(private http: HttpClient) { }
 
-
     ngOnChanges(changes: SimpleChanges): void {
-        if (changes['file']) 
+        if (changes['file'])
             this.http.get("/api/files/" + changes['file'].currentValue, { responseType: 'text' })
                 .subscribe(content => this.content = content);
     }
-    
+
 }
