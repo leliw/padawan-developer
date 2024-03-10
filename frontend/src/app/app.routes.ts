dac352ae2f72f4d8a2d1c870747cefb14345c502
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-03-10 12:52:03 +0100

    AppComponent moved to WorkspaceComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { WorkspacePageComponent } from './workspace/workspace-page/workspace-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'workspace', pathMatch: 'full' },
+    { path: 'workspace', component: WorkspacePageComponent }
+];
